091da10cac2b1fb766c4bbefd70a3caebb3b3bc3
strarsis <strarsis@gmail.com>
2015-09-17 20:13:13 +0200

    Fix dependency. Add sass wraper function.

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -7,7 +7,7 @@ module.exports  = function(eyeglass, sass) {
   return {
     sassDir: path.join(__dirname, 'sass'),
     functions: {
-      'inline-urlescape($text)': function(text, done) {
+      '_inline-urlescape($text)': function(text, done) {
         done(sass.types.String(inlineUrlescape( text.getValue() )));
       }
     }
